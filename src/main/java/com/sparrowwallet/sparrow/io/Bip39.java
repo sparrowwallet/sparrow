910dfcdeb15af7befbe02d3be652ae96c2ab120d
Craig Raw <craigraw@gmail.com>
2020-05-07 14:36:37 +0200

    bip39 keystore import

--- src/main/java/com/sparrowwallet/sparrow/io/Bip39.java
+++ src/main/java/com/sparrowwallet/sparrow/io/Bip39.java
@@ -1,6 +1,6 @@
 package com.sparrowwallet.sparrow.io;
 
-import com.sparrowwallet.drongo.protocol.ScriptType;
+import com.sparrowwallet.drongo.crypto.ChildNumber;
 import com.sparrowwallet.drongo.wallet.Bip39Calculator;
 import com.sparrowwallet.drongo.wallet.Keystore;
 import com.sparrowwallet.drongo.wallet.WalletModel;
@@ -8,12 +8,9 @@ import com.sparrowwallet.drongo.wallet.WalletModel;
 import java.util.List;
 
 public class Bip39 implements KeystoreMnemonicImport {
-
     @Override
-    public Keystore getKeystore(ScriptType scriptType, List<String> mnemonicWords, String passphrase) throws ImportException {
-        Bip39Calculator bip39Calculator = new Bip39Calculator();
-
-        return null;
+    public String getName() {
+        return "Mnemonic Words (BIP39)";
     }
 
     @Override
@@ -27,7 +24,9 @@ public class Bip39 implements KeystoreMnemonicImport {
     }
 
     @Override
-    public String getName() {
-        return null;
+    public Keystore getKeystore(List<ChildNumber> derivation, List<String> mnemonicWords, String passphrase) throws ImportException {
+        Bip39Calculator bip39Calculator = new Bip39Calculator();
+        byte[] seed = bip39Calculator.getSeed(mnemonicWords, passphrase);
+        return Keystore.fromSeed(seed, derivation);
     }
 }
--- src/main/java/com/sparrowwallet/sparrow/io/KeystoreMnemonicImport.java
+++ src/main/java/com/sparrowwallet/sparrow/io/KeystoreMnemonicImport.java
@@ -1,10 +1,10 @@
 package com.sparrowwallet.sparrow.io;
 
-import com.sparrowwallet.drongo.protocol.ScriptType;
+import com.sparrowwallet.drongo.crypto.ChildNumber;
 import com.sparrowwallet.drongo.wallet.Keystore;
 
 import java.util.List;
 
 public interface KeystoreMnemonicImport extends KeystoreImport {
-    Keystore getKeystore(ScriptType scriptType, List<String> mnemonicWords, String passphrase) throws ImportException;
+    Keystore getKeystore(List<ChildNumber> derivation, List<String> mnemonicWords, String passphrase) throws ImportException;
 }
