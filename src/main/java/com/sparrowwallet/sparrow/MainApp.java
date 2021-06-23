c8d997fbf0e2f7d1bde970adf941c0414ac29961
Craig Raw <craigraw@gmail.com>
2021-06-23 12:51:38 +0200

    v1.4.2

--- src/main/java/com/sparrowwallet/sparrow/MainApp.java
+++ src/main/java/com/sparrowwallet/sparrow/MainApp.java
@@ -32,7 +32,7 @@ public class MainApp extends Application {
     public static final String APP_ID = "com.sparrowwallet.sparrow";
     public static final String APP_NAME = "Sparrow";
     public static final String APP_VERSION = "1.4.2";
-    public static final String APP_VERSION_SUFFIX = "-beta";
+    public static final String APP_VERSION_SUFFIX = "";
     public static final String APP_HOME_PROPERTY = "sparrow.home";
     public static final String NETWORK_ENV_PROPERTY = "SPARROW_NETWORK";
 
