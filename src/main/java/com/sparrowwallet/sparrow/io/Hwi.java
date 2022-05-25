e2eb7d3fa92aa2f4c2013e27103e313aae7f9254
Craig Raw <craigraw@gmail.com>
2022-05-25 13:33:05 +0200

    hwi v2.1.1

--- src/main/java/com/sparrowwallet/sparrow/io/Hwi.java
+++ src/main/java/com/sparrowwallet/sparrow/io/Hwi.java
@@ -33,7 +33,7 @@ public class Hwi {
     private static final Logger log = LoggerFactory.getLogger(Hwi.class);
     private static final String HWI_HOME_DIR = "hwi";
     private static final String HWI_VERSION_PREFIX = "hwi-";
-    private static final String HWI_VERSION = "2.1.0";
+    private static final String HWI_VERSION = "2.1.1";
     private static final String HWI_VERSION_DIR = HWI_VERSION_PREFIX + HWI_VERSION;
 
     private static boolean isPromptActive = false;
