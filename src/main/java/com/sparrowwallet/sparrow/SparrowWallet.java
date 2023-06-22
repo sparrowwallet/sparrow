185a17edcefb8d9678e3fc721fe6c00a61a5f7ad
Craig Raw <craigraw@gmail.com>
2023-06-22 12:56:29 +0200

    bump to v1.7.8

--- src/main/java/com/sparrowwallet/sparrow/SparrowWallet.java
+++ src/main/java/com/sparrowwallet/sparrow/SparrowWallet.java
@@ -18,7 +18,7 @@ import java.util.*;
 public class SparrowWallet {
     public static final String APP_ID = "com.sparrowwallet.sparrow";
     public static final String APP_NAME = "Sparrow";
-    public static final String APP_VERSION = "1.7.7";
+    public static final String APP_VERSION = "1.7.8";
     public static final String APP_VERSION_SUFFIX = "";
     public static final String APP_HOME_PROPERTY = "sparrow.home";
     public static final String NETWORK_ENV_PROPERTY = "SPARROW_NETWORK";
