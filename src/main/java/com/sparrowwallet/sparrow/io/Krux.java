c9c0c3596453f093df30993b0f25572cba420759
Craig Raw <craigraw@gmail.com>
2023-10-05 10:35:55 +0200

    add krux as airgapped hww

---
+++ src/main/java/com/sparrowwallet/sparrow/io/Krux.java
@@ -0,0 +1,25 @@
+package com.sparrowwallet.sparrow.io;
+
+import com.sparrowwallet.drongo.wallet.WalletModel;
+
+public class Krux extends SpecterDIY {
+    @Override
+    public String getName() {
+        return "Krux";
+    }
+
+    @Override
+    public String getKeystoreImportDescription(int account) {
+        return "Import QR created on your Krux by selecting Extended Public Key from the main menu once you have loaded your mnemonic.";
+    }
+
+    @Override
+    public WalletModel getWalletModel() {
+        return WalletModel.KRUX;
+    }
+
+    @Override
+    public boolean isFileFormatAvailable() {
+        return false;
+    }
+}
--- src/main/java/com/sparrowwallet/sparrow/keystoreimport/HwAirgappedController.java
+++ src/main/java/com/sparrowwallet/sparrow/keystoreimport/HwAirgappedController.java
@@ -24,9 +24,9 @@ public class HwAirgappedController extends KeystoreImportDetailController {
     public void initializeView() {
         List<KeystoreFileImport> fileImporters = Collections.emptyList();
         if(getMasterController().getWallet().getPolicyType().equals(PolicyType.SINGLE)) {
-            fileImporters = List.of(new ColdcardSinglesig(), new CoboVaultSinglesig(), new Jade(), new KeystoneSinglesig(), new PassportSinglesig(), new SeedSigner(), new GordianSeedTool(), new SpecterDIY());
+            fileImporters = List.of(new ColdcardSinglesig(), new CoboVaultSinglesig(), new Jade(), new KeystoneSinglesig(), new PassportSinglesig(), new SeedSigner(), new GordianSeedTool(), new SpecterDIY(), new Krux());
         } else if(getMasterController().getWallet().getPolicyType().equals(PolicyType.MULTI)) {
-            fileImporters = List.of(new Bip129(), new ColdcardMultisig(), new CoboVaultMultisig(), new Jade(), new KeystoneMultisig(), new PassportMultisig(), new SeedSigner(), new GordianSeedTool(), new SpecterDIY());
+            fileImporters = List.of(new Bip129(), new ColdcardMultisig(), new CoboVaultMultisig(), new Jade(), new KeystoneMultisig(), new PassportMultisig(), new SeedSigner(), new GordianSeedTool(), new SpecterDIY(), new Krux());
         }
 
         for(KeystoreFileImport importer : fileImporters) {
