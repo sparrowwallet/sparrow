480ce1e4760a946a9aa56b5e227cf69b2c06bdc2
Craig Raw <craigraw@gmail.com>
2025-09-29 17:39:48 +0200

    fix deprecation warning

--- src/main/java/com/sparrowwallet/sparrow/io/db/DbPersistence.java
+++ src/main/java/com/sparrowwallet/sparrow/io/db/DbPersistence.java
@@ -171,7 +171,7 @@ public class DbPersistence implements Persistence {
 
     private synchronized void createUpdateExecutor(Wallet masterWallet) {
         if(updateExecutor == null) {
-            BasicThreadFactory factory = new BasicThreadFactory.Builder().namingPattern(masterWallet.getFullName() + "-dbupdater").daemon(true).priority(Thread.NORM_PRIORITY).build();
+            BasicThreadFactory factory = BasicThreadFactory.builder().namingPattern(masterWallet.getFullName() + "-dbupdater").daemon(true).priority(Thread.NORM_PRIORITY).build();
             updateExecutor = Executors.newSingleThreadExecutor(factory);
         }
     }
